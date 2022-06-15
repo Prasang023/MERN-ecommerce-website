3fa201062befeefab7ae35ddcbb50069ed13e2f3
Prasang023 <prasang023@gmail.com>
2022-06-15 21:03:35 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.send("API running..")
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', usersRoutes)
 app.use('/api/orders', orderRoutes)
@@ -35,8 +31,20 @@ app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
+
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => 
+            res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res)=>{
+        res.send("API running..")
+    })
+}
 
 app.use(notFound)
 
