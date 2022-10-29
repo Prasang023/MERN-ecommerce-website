d83d3ad2a0aa976e184ff22063394bd0b4194ff6
Averek <singhavanish2003@gmail.com>
2022-10-29 15:33:02 +0530

    about_page

---
+++ frontend/src/components/AboutUs.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <>
+      <div>
+        <h2>WHO WE ARE</h2>
+        <img src="" alt="" srcset="" />
+      </div>
+      <div>
+        <p>
+          We're an independently owned, strategic creative agency-forever
+          curiousand ready to transform the way business is done.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default AboutUs;
